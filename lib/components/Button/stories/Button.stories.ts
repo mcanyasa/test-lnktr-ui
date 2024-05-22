998a6cfa269ac8d374ff74eb41d91cd79c3e697f
mustafa.yasa <mustafa.yasa@dk.hepsipay.com>
2024-05-22 17:02:48 +0300

    first-template-init

---
+++ lib/components/Button/stories/Button.stories.ts
@@ -0,0 +1,66 @@
+import { MyButtonComponent } from "../../../index";
+import type { Meta, StoryObj } from "@storybook/react";
+import { fn } from "@storybook/test";
+
+const meta = {
+  title: "Example/Button",
+  component: MyButtonComponent,
+  parameters: {
+    layout: "centered",
+  },
+  tags: ["autodocs"],
+  argTypes: {
+    variant: {
+      control: {
+        type: "select",
+      },
+      options: [
+        "primary",
+        "secondary",
+        "destructive",
+        "outline",
+        "ghost",
+        "link",
+      ],
+    },
+  },
+  args: { onClick: fn() },
+} satisfies Meta<typeof MyButtonComponent>;
+export default meta;
+type Story = StoryObj<typeof meta>;
+export const Primary: Story = {
+  args: {
+    children: "Button",
+    variant: "default",
+  },
+};
+export const Secondary: Story = {
+  args: {
+    children: "Button",
+    variant: "secondary",
+  },
+};
+export const Destructive: Story = {
+  args: {
+    children: "Button",
+    variant: "destructive",
+  },
+};
+export const Outline: Story = {
+  args: {
+    children: "Button",
+    variant: "outline",
+  },
+};
+export const Ghost: Story = {
+  args: {
+    children: "Button",
+    variant: "ghost",
+  },
+};
+export const Link: Story = {
+  args: {
+    children: "Button",
+    variant: "link",
+  },
+};
